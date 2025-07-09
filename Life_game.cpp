606ec660494b4e73ea9d21e41c966fbeb405741a
Vadim <frank3671@yandex.ru>
2025-07-09 09:20:50 +0300

    feat: implement isAlive function

--- Life_game.cpp
+++ Life_game.cpp
@@ -8,6 +8,29 @@ inline int idx(int x, int y) {
     return y * GAME_WIDTH + x;
 }
 
+bool isAlive(const std::vector<int>& game, const int x, const int y)
+{
+    int alive = 0;
+
+    if (x > 0 && game[idx(x - 1, y)] == 1) alive += 1;
+    if (x < GAME_WIDTH - 1 && game[idx(x + 1, y)] == 1) alive += 1;
+    if (y > 0 && game[idx(x, y - 1)] == 1) alive += 1;
+    if (y < GAME_HEIGHT - 1 && game[idx(x, y + 1)] == 1) alive += 1;
+
+    if (y > 0 && x > 0 && game[idx(x - 1, y - 1)] == 1) alive += 1;
+    if (y > 0 && x < GAME_WIDTH - 1 && game[idx(x + 1, y - 1)] == 1) alive += 1;
+    if (y < GAME_HEIGHT - 1 && x > 0 && game[idx(x - 1, y + 1)] == 1) alive += 1;
+    if (y < GAME_HEIGHT - 1 && x < GAME_WIDTH - 1 && game[idx(x + 1, y + 1)] == 1) alive += 1;
+
+    if (game[idx(x, y)] == 1 && alive < 2) return false;
+    if (game[idx(x, y)] == 1 && (alive == 2 || alive == 3)) return true;
+    if (alive > 3) return false;
+    if (game[idx(x, y)] == 0 && alive == 3) return true;
+
+    return false;
+}
+
+
 int main() {
     G screen;
     std::vector<int> display(GAME_WIDTH * GAME_HEIGHT, 0);
