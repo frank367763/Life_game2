59ee3f4ef6e9176f564c1cf88364437e7de867d7
Vadim <frank3671@yandex.ru>
2025-07-09 09:17:33 +0300

    feat: add grid arrays for Game of Life

--- Life_game.cpp
+++ Life_game.cpp
@@ -1,11 +1,21 @@
 #include "screen.h"
+#include <vector>
+
+const int GAME_WIDTH = 640;
+const int GAME_HEIGHT = 480;
+
+inline int idx(int x, int y) {
+    return y * GAME_WIDTH + x;
+}
 
 int main() {
     G screen;
+    std::vector<int> display(GAME_WIDTH * GAME_HEIGHT, 0);
+    std::vector<int> swap(GAME_WIDTH * GAME_HEIGHT, 0);
+
     while (true) {
         screen.input();
         screen.clearpixels();
-        screen.drawpixel(100, 100);
         screen.update();
     }
     return 0;
